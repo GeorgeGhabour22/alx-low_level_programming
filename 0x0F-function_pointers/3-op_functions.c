34b0f2f6fb2b60f920bf0eec77c4dff703472861
George <georgeghabour22@gmail.com>
2023-11-09 13:35:08 -0800

    c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
  */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 /**
  * op_sub - Subtracts two integers.
@@ -17,7 +17,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 /**
  * op_mul - Multiplies two integers.
@@ -27,7 +27,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 /**
  * op_div - Divides two integers.
@@ -37,12 +37,12 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 /**
  * op_mod - Calculates the modulo of two integers.
@@ -52,11 +52,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
-
+return (a % b);
+}
